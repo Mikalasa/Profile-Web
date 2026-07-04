01568e4048bdd7794aec28b5152b9281e3f880b7
gezhe <44009421+Mikalasa@users.noreply.github.com>
2026-07-04 16:39:42 +1200

    testing cicd 5

--- mainWeb/src/constants/config-web-paragraph.js
+++ mainWeb/src/constants/config-web-paragraph.js
@@ -18,7 +18,7 @@ export const navLinks = [
 
 export const heroText = {
     title_f: "Hi, I'm ",
-    title_l: "Xingyi test 88again",
+    title_l: "Xingyi test 66again",
     description: <>
         {"Let's build the world."}
     </>
